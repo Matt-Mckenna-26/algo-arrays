8ad2f2be1ccd3acef08a41a8d5eadaac8a36a752
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-02-16 09:04:04 -0500

    array-1 practice

---
+++ ToDo1.js
@@ -0,0 +1,68 @@
+// Practicing simple algorithms 
+
+// Push Front
+// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+
+const arr = [1,2,3,4,5]
+const value='hi'
+
+
+// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+function pushFront(arr, value) {
+	// move all values in array over
+	for(let i = arr.length; i > 0; i--){
+		arr[i] = arr[i-1]
+    }
+	arr[0] = value;
+}
+
+
+// Pop Front
+//Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+function popFront(arr) {
+	const val = arr[0];
+	for(let i = 0; i < arr.length; i++)
+		arr[i] = arr[i + 1];
+	arr.length = arr.length - 1;
+	return val;
+}
+// Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. 
+// You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+const insertAt = (arr, val, idx) => {
+    for (let i = arr.length; i > idx ; i--){
+            arr[i] = arr[i-1]
+    }
+    arr[idx] = value;
+}
+
+
+// Given an array and an index into array, remove and return the array value at that index. Do this without using built-in array methods except pop(). 
+// Think of popFront(arr) as equivalent to removeAt(arr,0).
+function removeAt(arr, idx) {
+	toRemove = arr[idx];
+	for(let i = idx; i < arr.length-1; i++) {
+        arr[i] = arr[i+1];
+    }
+    arr.length = arr.length-1;
+    return toRemove;
+}
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. 
+// As with all array challenges, do this without using any built-in array methods.
+function swapPairs(arr) {
+	for(let i = 0; i < arr.length - 1; i = i + 2) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
+
+// Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. 
+function removeDupesUnnested(arr) {
+	let newArr = [];
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] !== arr[i+1]) 
+			newArr.push(arr[i])
+	}
+	return newArr;
+}
+
